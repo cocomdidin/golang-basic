ea14daeec6c17d5bf3607a9f99ed1037d11a3c9e
cocom <udn.chomr@gmail.com>
2022-04-22 05:55:20 +0700

    go run and build

---
+++ 2_goRunAndBuild/go.mod
@@ -0,0 +1,3 @@
+module goRunAndBuild
+
+go 1.18
---
+++ 2_goRunAndBuild/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// go rum hanya untuk menjalankan program, bukan untuk membuatnya.
+// contoh: go run main.go
+
+// go build membuat file executable.
+// contoh1: go build
+// contoh2: go build -o hello.exe
