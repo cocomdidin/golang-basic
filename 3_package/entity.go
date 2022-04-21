40eb4a78e95f90ce85c21e27dd96fbce49e68436
cocom <udn.chomr@gmail.com>
2022-04-22 06:06:00 +0700

    mengenal package pada golang

---
+++ 3_package/entity.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func TestAja() string {
+	return fmt.Sprintf("Hello nama saya %s umur %d, World!", "Komarudin", 20)
+}
---
+++ 3_package/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+
+	sentence := TestAja()
+
+	fmt.Println(sentence)
+}
+
+/*
+
+1. Package executable
+	aturan golang package executable harus bernama main
+2. Package library
+	package selain main
+
+*/
